abe3ac2a58057b9ede8014ba360bc1675da3ed2f
gandlasaisubramanyam <124264006+gandlasaisubramanyam@users.noreply.github.com>
2023-07-05 16:58:24 +0530

    Add files via upload

---
+++ JSON resume/script.js
@@ -0,0 +1,48 @@
+let myResume =
+{
+    "basics": {
+      "name": "SAI SUBRAMANYAM G",
+      "email": "gandlasai999@gamil.com",
+      "phone": 6300002787,
+      "degree": "B.tech",
+      "location": {
+        "address": "sreeramulapeta,near icici bank",
+        "postalCode": 515411,
+        "city": "anantapur",
+        "state": "AndhraPradesh",
+        "country": "India"
+      },
+    "education": [
+      {
+        "institution": "Srinivasa Ramanujan Institute of Technology",
+        "department":"computer science and engineering",
+        "studyType": "fulltime",
+        "batch start year": 2017,
+        "batch end year": 2022,
+        "gpa":"6.5",
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginner",
+        "project": [
+          " Fake currecy detection using image processing"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Telugu,Enlish",
+      }
+    ],
+    "Interests": [
+      {
+        "stock": "very much intrested in trading of stocks",
+      }
+    ]
+  }
+}
+  console.log(myResume)
+
+
---
+++ for loops/script.js
@@ -0,0 +1,33 @@
+var json = [{
+    "id" : "Boobalan", 
+    "msg"   : "For the given JSON iterate over all for loops (for, for in, for of, forEach)",
+    "task" : "zen class task",
+    "mail": "boobalans42@gmail.com"
+}];
+//for loop
+for(var i = 0; i < json.length; i++) {
+    var obj = json[i];
+
+    console.log(obj.id);
+    console.log(obj.msg);
+    console.log(obj.task);
+    console.log(obj.mail);
+
+}
+//for Each
+json.forEach(function(obj) { console.log(obj.msg); });
+
+//for In
+for (var key in json) {
+if (json.hasOwnProperty(key)) {
+  console.log(json[key].id);
+  //console.log(json[key].msg);
+ 
+}
+}
+//for Of
+let text = "";
+for (let x of json[key].id) {
+ text += x; 
+}
+ console.log(text);
